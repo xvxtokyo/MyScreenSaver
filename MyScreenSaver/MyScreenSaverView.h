f850f6b66f01799a66686f07519026e5fc6bd9e2
Shinya Nakashima <shinya.ore@gmail.com>
2020-01-02 20:52:16 +0900

    Initial Commit

---
+++ MyScreenSaver/MyScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  MyScreenSaverView.h
+//  MyScreenSaver
+//
+//  Created by Shinya Nakashima on 2020/01/02.
+//  Copyright © 2020 Shinya Nakashima. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MyScreenSaverView : ScreenSaverView
+
+@end
---
+++ MyScreenSaver/MyScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  MyScreenSaverView.m
+//  MyScreenSaver
+//
+//  Created by Shinya Nakashima on 2020/01/02.
+//  Copyright © 2020 Shinya Nakashima. All rights reserved.
+//
+
+#import "MyScreenSaverView.h"
+
+@implementation MyScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
